a4d2f6038d82f5e13b89081adef23c4921b93bd5
mstarski <32850577+sqr3byk@users.noreply.github.com>
2018-05-08 18:57:52 +0200

    Cleanup, added 'sale', user-friendly search

--- js/steam-api.js
+++ js/steam-api.js
@@ -1,10 +1,10 @@
-var appList = [];
-var filterList = [];
-var mapList = [];
-var gameList = [];
-
 angular.module("steamApi", ["angular.filter"])
     .controller("steamApiCtrl", function ($scope, $http, $filter) {
+        var appList = [];
+        var filterList = [];
+        var mapList = [];
+        var gameList = [];
+
         $scope.search = "";
         getAppList();
         $scope.$watch("search", function () {
@@ -12,16 +12,24 @@ angular.module("steamApi", ["angular.filter"])
         });
 
         function getAppList() {
-            $http.get("./data/steam-min.json")
+            $http.get("../data/steam-min.json")
                 .then(function (response) {
-                    appList = response.data.applist.apps;
+                    let resp = response.data.applist.apps;
+                    for (let i = 0; i < resp.length; i++) {
+                        appList.push({
+                            app: resp[i].app,
+                            name: resp[i].name,
+                            type: resp[i].type,
+                            search: (resp[i].name).replace(/[^\w\s-]/gi, '')
+                        });
+                    }
                 })
                 .catch(function (error) { })
         };
 
         function filterAppList() {
             if ($scope.search.length >= 3) {
-                filterList = $filter("filter")(appList, { name: $scope.search });
+                filterList = $filter("filter")(appList, { search: $scope.search });
                 mapList = filterList.map(id => id.app);
                 gameList = [];
                 getGameList();
@@ -43,15 +51,17 @@ angular.module("steamApi", ["angular.filter"])
                                     name: filterList[i].name,
                                     app: filterList[i].app,
                                     type: filterList[i].type,
-                                    price: (response.data[mapList[i]].data.price_overview.final / 100).toFixed(2)
-                                })
+                                    price: (response.data[mapList[i]].data.price_overview.final / 100).toFixed(2),
+                                    sale: (response.data[mapList[i]].data.price_overview.discount_percent != 0 ? true : false)
+                                });
                             } else {
                                 gameList.push({
                                     name: filterList[i].name,
                                     app: filterList[i].app,
                                     type: filterList[i].type,
-                                    price: "-"
-                                })
+                                    price: "-",
+                                    sale: false
+                                });
                             }
                         }
                     }
