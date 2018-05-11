00fd5494f4bdfba97be4a1cd3b06c5c00d690f4b
gege3pp5 <33030360+gege3pp5@users.noreply.github.com>
2018-05-11 04:11:07 +0200

    added dropdown menu to user account nav link

--- js/googleLogin.js
+++ js/googleLogin.js
@@ -1,8 +1,12 @@
 var userProfile = document.getElementById("account");
 		
-function onSuccess(googleUser) {
-	userProfile.innerHTML = '<a id="userProfile" class="nav-link" onclick="onFailure();">' + 
-						googleUser.getBasicProfile().getName() + ' &#9660;</a>';
+function onSuccess(googleUser) {				
+	userProfile.innerHTML = '<a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'
+						+ googleUser.getBasicProfile().getName() + '</a>'
+						+ '<div class="dropdown-menu dropdown-menu-right">'
+						+ '	<a class="dropdown-item">My account</a>'
+						+ '	<a class="dropdown-item" onclick="onFailure();">Logout</a>'
+						+ '</div>';
 }
 function onFailure() {
 	var auth2 = gapi.auth2.getAuthInstance();
