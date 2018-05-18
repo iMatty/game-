a15698d1e259d6e99e91559b4f203bca6dcdc789
gege3pp5 <33030360+gege3pp5@users.noreply.github.com>
2018-05-18 23:54:58 +0200

    fixed(swapped) light/dark mode text

--- js/dark_light.js
+++ js/dark_light.js
@@ -2,5 +2,5 @@ function toggleDarkLight() {
   var body = document.getElementById("body");
   var toggleButton = document.getElementById("darkLightToggle");
   body.className = body.className == "dark-mode" ? "light-mode" : "dark-mode";
-  toggleButton.innerHTML = body.className == "dark-mode" ? "dark" : "light";
+  toggleButton.innerHTML = body.className == "dark-mode" ? "light mode" : "dark mode";
 }
