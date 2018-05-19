d19e7ded085b5ed837360a401978dead25d6dc3e
gege3pp5 <33030360+gege3pp5@users.noreply.github.com>
2018-05-19 17:17:48 +0200

    fixed dark/light toggle button text toggle

--- js/dark_light.js
+++ js/dark_light.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
     if($("body").hasClass("light-mode")) {
       $("body").removeClass("light-mode").addClass("dark-mode");
       $("#gamesTable").toggleClass("table-dark");
+	  $("#darkLightToggle").text("light mode");
     } else {
       $("body").removeClass("dark-mode").addClass("light-mode");
       $("#gamesTable").toggleClass("table-dark");
+	  $("#darkLightToggle").text("dark mode");
     }
   });
 });
