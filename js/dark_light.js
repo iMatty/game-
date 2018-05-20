4e28db207bcaeb067cddff52124fb2d29b6e78bb
mstarski <32850577+sqr3byk@users.noreply.github.com>
2018-05-20 16:47:18 +0200

    Added dynamic tooltip title

--- js/dark_light.js
+++ js/dark_light.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip({
-    trigger: 'hover'
+    trigger: "hover"
   });
   $("#darkLightToggle").click(function () {
     if ($("body").hasClass("light-mode")) {
       $("body").removeClass("light-mode").addClass("dark-mode");
       $("#gamesTable").toggleClass("table-dark");
+      $(this).attr('data-original-title', "Toggle light mode").tooltip('show');
     } else {
       $("body").removeClass("dark-mode").addClass("light-mode");
       $("#gamesTable").toggleClass("table-dark");
+      $(this).attr('data-original-title', "Toggle dark mode").tooltip('show');
     }
   });
 });
