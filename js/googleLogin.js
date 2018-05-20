63775340e0454b1aeadcea1fbc539124978ee708
gege3pp5 <33030360+gege3pp5@users.noreply.github.com>
2018-05-20 23:32:17 +0200

    fixed login

--- js/googleLogin.js
+++ js/googleLogin.js
@@ -4,7 +4,6 @@ function onSuccess(googleUser) {
 	userProfile.innerHTML = '<a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'
 						+ googleUser.getBasicProfile().getName() + '</a>'
 						+ '<div class="dropdown-menu dropdown-menu-right">'
-						+ '	<a class="dropdown-item">My account</a>'
 						+ '	<a class="dropdown-item" onclick="onFailure();">Logout</a>'
 						+ '</div>';
 }
